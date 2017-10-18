1af791488fab454b9a5529420053af0d2162d786
ChuckBTaylor <WLScripps@gmail.com>
2017-10-18 10:49:45 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: %i[show edit update]
 
   def index
     @posts = Post.all
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @post}
+      format.json { render json: @post }
     end
   end
 
@@ -18,12 +18,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to post_path(@post)
+    if params[userInput]
+      @post = Post.new(params[userInput])
+      return (render json: @post, status: 201) if @post.save
+    end
+    render :new
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @post.update(post_params)
@@ -32,12 +34,13 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    #render json: PostSerializer.serialize(post)
-    render json: post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}])
+    # render json: PostSerializer.serialize(post)
+    render json: post.to_json(only: %i[title description id],
+                              include: [author: { only: [:name] }])
   end
 
-private
+  private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
